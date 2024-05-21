01fa0034a96b7e4aae4a5605f872eb9b1349eec9
Tapas Kumar Sahu <tapassahu04@gmail.com>
2024-05-21 10:40:07 +0530

    add store

--- store.js
+++ store.js
@@ -1,19 +1,21 @@
 const store = {
     todos: [
         {
-            "id": 1,
+            "id": "1",
             "title": "Complete Task 1",
             "completed": false,
         },
         {
-            "id": 2,
+            "id": "2",
             "title": "Complete Task 2",
             "completed": true,
         },
         {
-            "id": 3,
+            "id": "3",
             "title": "Complete Task 3",
             "completed": false,
         }
     ],
 }
+
+export default store;
