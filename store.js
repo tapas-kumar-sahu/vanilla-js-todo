f49912ff2a1a5f9a39dbbaf320e3482561240a00
Tapas Kumar Sahu <tapassahu04@gmail.com>
2024-05-21 10:34:59 +0530

    add store

---
+++ store.js
@@ -0,0 +1,19 @@
+const store = {
+    todos: [
+        {
+            "id": 1,
+            "title": "Complete Task 1",
+            "completed": false,
+        },
+        {
+            "id": 2,
+            "title": "Complete Task 2",
+            "completed": true,
+        },
+        {
+            "id": 3,
+            "title": "Complete Task 3",
+            "completed": false,
+        }
+    ],
+}
