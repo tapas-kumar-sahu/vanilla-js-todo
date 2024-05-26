50040c322bfb418f26eda1befd0687aab7076035
Tapas Kumar Sahu <tapassahu04@gmail.com>
2024-05-26 20:57:58 +0530

    add crud

--- app.js
+++ app.js
@@ -1,3 +1,54 @@
 import render from "./render.js";
+import store from "./store.js";
+import { addTodo, deleteTodo, toggleCompleted } from "./store.js";
 
-render();
+window.addEventListener("todoschange", () => {
+    render();
+})
+
+// initial render
+// render();
+// try to get store from local storage
+const storeFromLocalStorage = JSON.parse(localStorage.getItem("store"));
+if (storeFromLocalStorage?.todos.length > 0) {
+    store.todos = storeFromLocalStorage.todos;
+}
+else {
+    localStorage.setItem("store", JSON.stringify(store));
+    render();
+}
+
+
+
+const form = document.querySelector("#form");
+const todoTitleInput = document.querySelector(".todo-title-input");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const todoTitle = todoTitleInput.value;
+    const newTodo = {
+        id: crypto.randomUUID(),
+        title: todoTitle,
+        completed: false
+    }
+    addTodo(newTodo);
+})
+
+const todos = document.querySelector(".todos");
+todos.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.classList.contains("delete-todo-button")) {
+        const id = target.closest(".todo").dataset.id;
+        deleteTodo(id);
+    }
+})
+
+todos.addEventListener("change", (e) => {
+    const target = e.target;
+    if (target.classList.contains("todo-checkbox")) {
+        const id = target.closest(".todo").dataset.id;
+        const completed = e.target.checked;
+        toggleCompleted(id, completed);
+    }
+
+})
--- store.js
+++ store.js
@@ -16,6 +16,49 @@ const store = {
             "completed": false,
         }
     ],
+    tasks: [
+        {
+            "id": "1",
+            "title": "My Task 1",
+            "completed": false,
+        },
+    ]
 }
 
-export default store;
+const storeHandler = {
+    // traps
+    get(target, property) {
+        return target[property]
+    },
+    set(target, property, value) {
+        target[property] = value;
+        if (property == "todos") {
+            window.dispatchEvent(new Event("todoschange"));
+        }
+        localStorage.setItem("store", JSON.stringify(store));
+        return true;
+    }
+}
+
+const storeProxy = new Proxy(store, storeHandler);
+
+const addTodo = (newTodo) => {
+    storeProxy.todos = [...storeProxy.todos, newTodo];
+}
+
+const deleteTodo = (id) => {
+    storeProxy.todos = storeProxy.todos.filter((todo) => todo.id !== id)
+}
+
+const toggleCompleted = (id, completed) => {
+    storeProxy.todos = storeProxy.todos.map((todo) => {
+        if (todo.id === id) {
+            return { ...todo, completed: completed };
+        } else {
+            return todo;
+        }
+    })
+}
+
+export { addTodo, deleteTodo, toggleCompleted };
+export default storeProxy;
